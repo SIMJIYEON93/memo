4624446001bd46def143985ae85ff54000c534a4
SIMJIYEON93 <930515s@gmail.com>
2023-06-28 10:39:17 +0900

    update, delete api

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -43,6 +43,35 @@ public class MemoController {
                     .map(MemoResponseDto::new).toList();
 
            return responseList;
+    }
 
+    @PutMapping("/memos/{id}") //update, pathVariable 방식
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){ //반환은 업데이트 한 id값만
+        //update할 id -> pathVariavble
+        // httpbody부분에 json형태의 수정된 data -> requestDto
+
+        //해당 메목 db에 존재하는지 확인
+        if(memoList.containsKey(id)) {
+            //해당 메모 가지고 오기
+            Memo memo = memoList.get(id);
+
+            //메모 수정
+            memo.update(requestDto);
+            return memo.getId();
+
+        }else{
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id){
+        //해당 메모가 존재하는지 확인
+        if(memoList.containsKey(id)){
+        memoList.remove(id);
+        return id;
+    }else{
+            throw new IllegalArgumentException("해당 메모는 존재하지 않습니다.");
+        }
+}
 }
