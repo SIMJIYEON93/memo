ecc37e5a05945799cf80e15c1d9555e70594f73e
SIMJIYEON93 <930515s@gmail.com>
2023-06-28 22:42:05 +0900

    JPA Auditing

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -3,21 +3,23 @@ package com.sparta.memo.dto;
 import com.sparta.memo.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto { //memo entityclass와 비슷해
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
 
     public MemoResponseDto(Memo memo) { //memo에게 받아온 데이터를 reponseDto에 넣어주기 위해
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
-    }
-
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id =id;
-        this.username=username;
-        this.contents =contents;
+        this.createdAt=memo.getCreatedAt();
+        this.modifiedAt=memo.getModifiedAt();
     }
 }
